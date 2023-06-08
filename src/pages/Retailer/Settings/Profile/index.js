e7f646c7b7dadbc62efa3c230d3780782e382db8
Aamir <aamir@arthagri.in>
2023-06-08 17:51:17 +0530

    SHOPDOT-819-Resolved

--- src/pages/Retailer/Settings/Profile/index.js
+++ src/pages/Retailer/Settings/Profile/index.js
@@ -66,6 +66,7 @@ export default function RetailerProfile() {
   const brandCategoryList = useSelector(selectBrandCategory);
   const brandProfileDetails = useSelector(selectBrandProfileDetails);
   const [profileLoading, setProfileLoading] = useState(false);
+  const [selectedRetailerValues, setSelectedRetailerValues] = useState([]);
   let transformCategoryOptions = [];
   if (brandCategoryList && brandCategoryList.length > 0) {
     transformCategoryOptions = brandCategoryList?.map((el) => {
@@ -172,6 +173,7 @@ export default function RetailerProfile() {
       await map(brandData.retailer_details.retailer_values, (cat, key) => {
         valuesArray.push(cat.value_id);
       });
+      setSelectedRetailerValues(valuesArray)
     }
     if (brandData?.retailer_details?.id) {
       setImage(brandData?.retailer_details?.store_logo || '');
@@ -284,6 +286,7 @@ export default function RetailerProfile() {
       aboutTheRetailer: brandProfileDetails?.retailer_details?.retailer_story,
       countryAddress: getDefaultValueOfCountryField(),
       stateAddress: getDefaultValueOfStateField(),
+      retailer_promo: brandProfileDetails?.retailer_details?.retailer_promo,
     },
     mode: 'onChange',
     resolver: yupResolver(retailerProfileValidationSchema),
@@ -352,6 +355,7 @@ export default function RetailerProfile() {
       city: data.city,
       zip: data.zipcode,
       store_mailing_address: 'test address',
+      retailer_promo: data.retailer_promo
     };
     console.log('profileData----', profileData);
     if (brandProfileDetails) {
@@ -771,6 +775,7 @@ export default function RetailerProfile() {
                                         <input
                                           type="checkbox"
                                           name={'retialerValue'}
+                                          checked={selectedRetailerValues.includes(val.id) ? "checked" : '' }
                                           value={val.id}
                                           {...register('retialerValue', {
                                             required: false,
@@ -813,14 +818,14 @@ export default function RetailerProfile() {
                           <input
                             type="text"
                             className="form-control"
-                            name="link"
-                            {...register('link', {
+                            name="retailer_promo"
+                            {...register('retailer_promo', {
                               required: false,
                             })}
                           />
-                          {errors.link && (
+                          {errors.retailer_promo && (
                             <span className="error-text">
-                              {errors.link?.message}
+                              {errors.retailer_promo?.message}
                             </span>
                           )}
                         </div>
