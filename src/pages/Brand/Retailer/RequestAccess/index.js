95d35607e3f91807f2ef9f6c62c14ff65c468014
Aamir <aamir@arthagri.in>
2023-06-21 21:18:16 +0530

    SHOPDOT-777-Resolved

--- src/pages/Brand/Retailer/RequestAccess/index.js
+++ src/pages/Brand/Retailer/RequestAccess/index.js
@@ -139,7 +139,6 @@ export default function RequestAccess(props) {
     return statusText;
   }
   const getStatus = (status) => {
-    console.log(status);
     if(status == "accepted"){
       status = "connected"
     }
